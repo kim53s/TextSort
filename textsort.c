b9e2171d1d35cc6d9424571610ad7a27eda8e541
Sujin Kim <kim53s@mtholyoke.edu>
2018-02-09 13:29:42 -0500

    check arguments
    check if the arguments are valid or not

--- textsort.c
+++ textsort.c
@@ -2,15 +2,24 @@
 #include <stdlib.h>
 
 void readFile(char file[]);
+int compare(const void *a, const void *b);
 
 int main(int argc, char **argv){
 
 
 	if(argc > 3 || argc == 1){
 		fprintf(stderr, "Error: Bad command line parameters\n");
-		return 1;
+		exit(1);
 	}
-	else
+	else if( argc == 3 ){
+		int num;
+		char *end;
+		num = strtol(argv[2], &end, 10);
+		if(num >= 0 ){
+			fprintf(stderr, "Error: Bad command line parameters\n");
+			exit(1);
+		}
+	}
 		readFile(argv[1]);
 
 	return 0;
@@ -22,8 +31,9 @@ void readFile(char file[]){
 
 	f = fopen(file, "rb"); 
 	if(f == NULL){
-		printf("NULL!!");
+		fprintf(stderr, "There is no such file.\n");
 	}
+
 	ch = getc(f);
 	while (ch != EOF){
 		putchar(ch);
@@ -32,3 +42,18 @@ void readFile(char file[]){
 	printf("\n");
 	fclose(f);
 }
+
+// int compare(const void *a, const void *b){
+// 	/* Cast to its actual type. */
+//     char **strptr1 = (char **) elem1;
+//     char **strptr2 = (char **) elem2;
+    
+//      Dereference to get the strings 
+//     char *str1 = *strptr1;
+//     char *str2 = *strptr2;
+
+//     /* Then use strcmp to compare the strings */
+
+// 	return ( *(char *)a - *(char *)b);
+// }
+
