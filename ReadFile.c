3d642c58b3e93da3b65d331beb84fd158a21371f
zl2359 <liang22z@mtholyoke.edu>
2018-02-13 20:53:35 -0500

    Update ReadFile.c

--- ReadFile.c
+++ ReadFile.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-int a;
-int b;
-
+#include <ctype.h>
 
 void readFile(char file[]);
 int compare(const void *a, const void *b);
+char* getWord(char *string);
+
+int maxRow = 10;
+int row = 1;
+char **strs;
+int wordNum;
+
+int checkInput(char input[]){
+    int i = 0;
+    for (i=1; input[i]!=0; i++){
+        if (!isdigit(input[i])){
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int main(int argc, char **argv){
     
@@ -20,83 +33,154 @@ int main(int argc, char **argv){
     else if( argc == 3 ){
         int num;
         char *end;
-        num = strtol(argv[2], &end, 10);
         
-        // check the second argument meets the format of a dash followed by a number
+        if (checkInput(argv[1])==1){
+            fprintf(stderr, "Error: Bad command line parameters\n");
+        }
+      
+        
+        else{
+            num = strtol(argv[1], &end, 10);
+        
+        // check the first argument meets the format of a dash followed by a number
         if(num >= 0 ){
             fprintf(stderr, "Error: Bad command line parameters\n");
             exit(1);
         }
+        else{
+            wordNum = -num;
+            readFile(argv[2]);
+            return 0;
+        }
+        }
     }
-    
-    // read the file
-    readFile(argv[1]);
-    
+    else{  // there is only one argument
+        wordNum = 1;
+        readFile(argv[1]); // read the file
+    }
+
+    //printf("%d\n", wordNum);
     return 0;
 }
 
 
-
-void sortString(char *str[a][b]){
-    printf("%s\n", str[0][0]);
-    int i;
-    char words[a];
-    for (i=0;i<=a;i++){
-     words[i] = *str[i][0];
-         //printf("%s\n", words[i]);
-    }
-}
-
-
-
-
 void readFile(char file[]){
     FILE *f;
-
-
-    int i=0;
-
+    int ch;
     
     f = fopen(file, "rb");
     if(f == NULL){
-        fprintf(stderr, "There is no such file.\n");
+        fprintf(stderr, "Error: Cannot open file %s\n", file);
         exit(1);
     }
     
     
-    char input[100][100];
-    
-    char *str[sizeof(input)][sizeof(input[0])];
-    
-    
-    while(fgets(input[i],sizeof(input),f)){
-        input[i][strlen(input[i]) - 1] = '\0';
-        i++;
+    strs = (char**)malloc(sizeof(char*)*maxRow);
+    for(int i = 0; i < maxRow; i++){
+        strs[i] = (char*)malloc(sizeof(char)*128);
     }
     
-    
-    char * pch;
-    int j;
-    int k;
-    for(k=0; k<sizeof(str); k++){
-        pch = strtok (input[k]," ");
-        j=0;
-        while (pch != NULL)
-        {
-            str[k][j] = pch;
-            //printf("%s\n", str[k][j]);
-            j++;
-            pch = strtok (NULL, " ");
+    ch = getc(f);
+    while (ch != EOF){
+        if(ch != '\n'){
+            if(strlen(strs[row-1]) < 128){
+                char temp[2];
+                temp[0] = ch;
+                temp[1] = '\0';
+                strcat(strs[row-1], temp);
+            }
+            else{
+                fprintf(stderr, "Line too long\n");
+                exit(1);
+            }
         }
-        printf("%s\n", str[1][1]);
+        else{
+            row++;
+            
+            // reallocate
+            if(row > maxRow){
+                char** strs2 = (char**)realloc(strs, sizeof(char*)*maxRow*2);
+                
+                if(strs2 != NULL){
+                    strs = strs2;
+                }
+                else{
+                    fprintf(stderr, "malloc failed\n");
+                    exit(1);
+                }
+                
+                for(int i = maxRow; i < maxRow*2; i++){
+                    strs[i] = (char*)malloc(sizeof(char)*128);
+                }
+                
+                maxRow = maxRow*2;
+            }
+        }
+        ch = getc(f);
     }
-    printf("%s\n", str[1][1]);
-   printf("%s\n", "hello world");
-    sortString(str);
+    
+    qsort((char *)strs, row, sizeof(strs[0]), compare);
+    
+    // print out the sorted sentences
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < strlen(strs[i]); j++){
+            printf("%c", strs[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for(int i = 0; i < maxRow; i++){
+        free(strs[i]);
+    }
+    free(strs);
     
     fclose(f);
 }
 
+char* getWord(char *string){
+    
+   	const char s[2] = " ";
+   	char *token;
+    char *former;
+   	// the first token
+   	token = strtok(string, s);
+    
+   	for(int i = 0; i < wordNum-1; i++){
+        former = token;
+        token = strtok(NULL, s);
+        if(token == NULL){
+            return former;
+        }		
+   	}
+
+   	return token;
+}
 
 
 
+int compare(const void *elem1, const void *elem2){
+    // Cast to its actual type.
+    char **strptr1 = (char **) elem1;
+    char **strptr2 = (char **) elem2;
+   	
+    // Dereference to get the strings 
+    char *str1 = *strptr1;
+    char *str2 = *strptr2;
+    
+    char s1[sizeof(char)*strlen(str1)];
+    char s2[sizeof(char)*strlen(str2)];
+    
+    for(int i = 0; i < strlen(str1); i++){
+        s1[i] = str1[i]; 
+    }	
+    
+    for(int i = 0; i < strlen(str2); i++){
+        s2[i] = str2[i]; 
+    }	
+    
+    char *word1 = getWord(s1);
+    char *word2 = getWord(s2);
+    
+    /* Then use strcmp to compare the strings */
+    return strcmp(word1, word2);
+}
